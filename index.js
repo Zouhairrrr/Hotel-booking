a0c279280cfcef31cc225f7f7b79b05d04c0d8dc
Zouhairrrr <77061268+Zouhairrrr@users.noreply.github.com>
2022-01-28 11:51:08 +0100

    commit admin routes

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require('express')
+const adminRoute = require('./router/admin/adminRoute')
+
+
 
 const app = express();
 app.use(express.json())
 
 
 
+// * Admin route
+app.use('/dashnbard/admin', adminRoute);
 
 
-// app.use(bodyParser.json());
 
-// app.use('/api/user', userRoute);
+
 // app.use('/api/hotel', hotelRoute);
 // app.use('/api/room', roomRoute);
 // app.use('/api/booking', bookingRoute);
---
+++ router/admin/adminRoute.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const AdminRoute  = express.Router();
+
+
+AdminRoute.post('/', async (req, res) => { adminController.InsertAdmin(req, res); });
+
+
+AdminRoute.get('/', () => require())
