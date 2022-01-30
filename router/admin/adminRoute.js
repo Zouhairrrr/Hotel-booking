664376674262e1c9bff67b089bd66830e26dd312
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2022-01-30 21:16:11 +0100

    Export admin file

--- router/admin/adminRoute.js
+++ router/admin/adminRoute.js
@@ -5,4 +5,6 @@ const AdminRoute  = express.Router();
 AdminRoute.post('/', async (req, res) => { adminController.InsertAdmin(req, res); });
 
 
-AdminRoute.get('/', () => require())
+AdminRoute.get('/', () => require());
+
+module.exports = AdminRoute;
