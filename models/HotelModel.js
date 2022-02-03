ea2db230f242bbe50f026682c7cc86abb50cb79e
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2022-02-03 11:16:04 +0100

    Modification on the schema

--- models/HotelModel.js
+++ models/HotelModel.js
@@ -1,6 +1,6 @@
-const mongoose = require('mongoose'); 
-var  hotelSchema = new mongoose.Schema({
-    name:{
+const mongoose = require('mongoose');
+var hotelSchema = new mongoose.Schema({
+    name: {
         type: String,
         required: [true, 'Hotel must have a name']
     },
@@ -9,14 +9,31 @@ var  hotelSchema = new mongoose.Schema({
         required: [true, 'Hotel must have a description']
     },
     localisation: {
-        type: String,
-        required: [true, 'Hotel must have a localisation']
+        city: {
+            type: String,
+            required: [true, 'Hotel must have a city']
+        },
+        country: {
+            type: String,
+            required: [true, 'Hotel must have a coutry']
+        }
     },
-    // imageCover :{
+    price: {
+        type: String,
+        required: [true, 'Hotel must have a price']
+    },
+    starts: {
+        type: String,
+        required: [true, 'Hotel must have a stars']
+    },
+    // imageCover: {
     //     type: String,
-    //     required: [true, 'Hotel must have an image']
+    //     required: [true, 'Hotel must have a cover image']
     // },
-    // images: [String]
+    // images: [{
+    //     type: String,
+    //     required: [true, 'Hotel must have at least 4 images']
+    // }],
 });
 
 const Hotel = mongoose.model('hotel', hotelSchema);
