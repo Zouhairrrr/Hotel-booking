8b705c55dfbae2de87b008d1a31bcef730fb14c2
Ziadov37 <77000963+Ziadov37@users.noreply.github.com>
2022-02-07 16:48:14 +0100

    Modification on upload

--- controllers/HotelController.js
+++ controllers/HotelController.js
@@ -1,50 +1,15 @@
 const console = require("console");
 const Hotel = require("../models/HotelModel");
-// const multer = require('multer');
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, './uploads/');
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, new Date().toISOString() + file.originalname);
-//     },
-// });
-// const uploadImage = multer({
-//     storage: storage
-// }).single('imageCover');
 
-// Upload Image (Doesn't Work !!)
-// exports.uploadImage = async (req, res, next) => {
-//     try {
-//         const imageCover = await imageCover.uploads({
-
-//             imageCover: req.file.path,
-
-//         });
-
-//         res.status(200).json({
-//             status: "success Upload",
-//             // data: imageCover
-//         });
-
-//     } catch (err) {
-//         console.log(err);
-//     }
-//     next();
-
-// }
 // Create Hotel
 exports.createHotel = async (req, res, next) => {
-    // console.log(req.body.localisation);
     const images = req.files.map((file) => {
         return file.path
     })
-    // return res.send(images)
     const {
         city,
         country
     } = req.body.localisation
-    // console.log(req.body.stars);
     try {
         const hotel = await Hotel.create({
             name: req.body.name,
@@ -55,7 +20,6 @@ exports.createHotel = async (req, res, next) => {
             },
             price: req.body.price,
             starts: req.body.stars,
-            // imageCover: req.file.path,
             images: images,
         });
 
@@ -66,7 +30,6 @@ exports.createHotel = async (req, res, next) => {
     } catch (err) {
         console.log(err);
     }
-
 }
 
 // Get one hotel by ID
